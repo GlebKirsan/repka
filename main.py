834a98e5d17b3ac31946ed38b884e3ed6191808e
AlexaEgorova <egorova.a007@gmail.com>
2020-04-14 21:55:52 +0300

    tone's updated, .py files created

---
+++ main.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(430, 300)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(19, 19, 392, 231))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.mainVerticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.mainVerticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.mainVerticalLayout.setObjectName("mainVerticalLayout")
+        self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+        self.mainVerticalLayout.addWidget(self.label)
+        self.nameHorizontalLayout = QtWidgets.QHBoxLayout()
+        self.nameHorizontalLayout.setObjectName("nameHorizontalLayout")
+        self.nameLineEdit = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.nameLineEdit.setMaximumSize(QtCore.QSize(300, 16777215))
+        self.nameLineEdit.setObjectName("nameLineEdit")
+        self.nameHorizontalLayout.addWidget(self.nameLineEdit)
+        self.nameLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.nameLabel.setObjectName("nameLabel")
+        self.nameHorizontalLayout.addWidget(self.nameLabel)
+        self.mainVerticalLayout.addLayout(self.nameHorizontalLayout)
+        self.authorHorizontalLayout = QtWidgets.QHBoxLayout()
+        self.authorHorizontalLayout.setObjectName("authorHorizontalLayout")
+        self.authorLineEdit = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.authorLineEdit.setMinimumSize(QtCore.QSize(0, 0))
+        self.authorLineEdit.setMaximumSize(QtCore.QSize(300, 16777215))
+        self.authorLineEdit.setObjectName("authorLineEdit")
+        self.authorHorizontalLayout.addWidget(self.authorLineEdit)
+        self.authorLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.authorLabel.setObjectName("authorLabel")
+        self.authorHorizontalLayout.addWidget(self.authorLabel)
+        self.mainVerticalLayout.addLayout(self.authorHorizontalLayout)
+        self.buttonHorizontalLayout = QtWidgets.QHBoxLayout()
+        self.buttonHorizontalLayout.setObjectName("buttonHorizontalLayout")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.buttonHorizontalLayout.addItem(spacerItem)
+        self.analyseButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.analyseButton.setMinimumSize(QtCore.QSize(100, 0))
+        self.analyseButton.setObjectName("analyseButton")
+        self.buttonHorizontalLayout.addWidget(self.analyseButton)
+        self.mainVerticalLayout.addLayout(self.buttonHorizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 430, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Для анализа эмоциональной окраски музыкального произведения\n"
+" введите название и исполнителя и нажмите \"Анализировать\""))
+        self.nameLabel.setText(_translate("MainWindow", "Название"))
+        self.authorLabel.setText(_translate("MainWindow", "Исполнитель"))
+        self.analyseButton.setText(_translate("MainWindow", "Анализировать"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ tone.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'tone.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(430, 300)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(19, 19, 392, 235))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.mainVerticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.mainVerticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.mainVerticalLayout.setObjectName("mainVerticalLayout")
+        self.infoHorizontalLayout = QtWidgets.QHBoxLayout()
+        self.infoHorizontalLayout.setObjectName("infoHorizontalLayout")
+        self.imageLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.imageLabel.setMinimumSize(QtCore.QSize(0, 150))
+        self.imageLabel.setText("")
+        self.imageLabel.setObjectName("imageLabel")
+        self.infoHorizontalLayout.addWidget(self.imageLabel)
+        self.commonInfoVerticalLayout = QtWidgets.QVBoxLayout()
+        self.commonInfoVerticalLayout.setObjectName("commonInfoVerticalLayout")
+        self.yearHorizontalLayout = QtWidgets.QHBoxLayout()
+        self.yearHorizontalLayout.setObjectName("yearHorizontalLayout")
+        self.textYearLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.textYearLabel.setMaximumSize(QtCore.QSize(30, 16777215))
+        self.textYearLabel.setObjectName("textYearLabel")
+        self.yearHorizontalLayout.addWidget(self.textYearLabel)
+        self.yearLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.yearLabel.setText("")
+        self.yearLabel.setObjectName("yearLabel")
+        self.yearHorizontalLayout.addWidget(self.yearLabel)
+        self.commonInfoVerticalLayout.addLayout(self.yearHorizontalLayout)
+        self.authorHorizontalLayout = QtWidgets.QHBoxLayout()
+        self.authorHorizontalLayout.setObjectName("authorHorizontalLayout")
+        self.textAuthorLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.textAuthorLabel.setMaximumSize(QtCore.QSize(75, 16777215))
+        self.textAuthorLabel.setObjectName("textAuthorLabel")
+        self.authorHorizontalLayout.addWidget(self.textAuthorLabel)
+        self.authorLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.authorLabel.setText("")
+        self.authorLabel.setObjectName("authorLabel")
+        self.authorHorizontalLayout.addWidget(self.authorLabel)
+        self.commonInfoVerticalLayout.addLayout(self.authorHorizontalLayout)
+        self.nameHorizontalLayout = QtWidgets.QHBoxLayout()
+        self.nameHorizontalLayout.setObjectName("nameHorizontalLayout")
+        self.textNameLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.textNameLabel.setMaximumSize(QtCore.QSize(60, 16777215))
+        self.textNameLabel.setObjectName("textNameLabel")
+        self.nameHorizontalLayout.addWidget(self.textNameLabel)
+        self.nameLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.nameLabel.setText("")
+        self.nameLabel.setObjectName("nameLabel")
+        self.nameHorizontalLayout.addWidget(self.nameLabel)
+        self.commonInfoVerticalLayout.addLayout(self.nameHorizontalLayout)
+        self.infoHorizontalLayout.addLayout(self.commonInfoVerticalLayout)
+        self.mainVerticalLayout.addLayout(self.infoHorizontalLayout)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.textMoodLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.textMoodLabel.setMaximumSize(QtCore.QSize(100, 16777215))
+        self.textMoodLabel.setObjectName("textMoodLabel")
+        self.horizontalLayout.addWidget(self.textMoodLabel)
+        self.moodLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.moodLabel.setText("")
+        self.moodLabel.setObjectName("moodLabel")
+        self.horizontalLayout.addWidget(self.moodLabel)
+        self.mainVerticalLayout.addLayout(self.horizontalLayout)
+        self.buttonHorizontalLayout = QtWidgets.QHBoxLayout()
+        self.buttonHorizontalLayout.setObjectName("buttonHorizontalLayout")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.buttonHorizontalLayout.addItem(spacerItem)
+        self.moreButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.moreButton.setMinimumSize(QtCore.QSize(100, 0))
+        self.moreButton.setObjectName("moreButton")
+        self.buttonHorizontalLayout.addWidget(self.moreButton)
+        self.mainVerticalLayout.addLayout(self.buttonHorizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 430, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.textYearLabel.setText(_translate("MainWindow", "Год:"))
+        self.textAuthorLabel.setText(_translate("MainWindow", "Исполнитель:"))
+        self.textNameLabel.setText(_translate("MainWindow", "Название:"))
+        self.textMoodLabel.setText(_translate("MainWindow", "Общее настроение:"))
+        self.moreButton.setText(_translate("MainWindow", "Подробнее"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
