6f1a21683fbe6e49c3e135c15fd2a90ff1e23b94
GlebKirsan <glkv@protonmail.ch>
2020-05-01 10:51:13 +0300

    Added lyrics finder file

---
+++ lyrics_finder.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+
+def get_lyrics(author: str, title: str) -> str:
+    try:
+        options = webdriver.ChromeOptions()
+        # Параметр для браузера, чтобы не открывалось окно
+        options.add_argument('headless')
+        driver = webdriver.Chrome(chrome_options=options)
+        
+        # Сайт с текстами песен
+        driver.get('https://lyricsworld.ru/')
+        
+        # Через селектор находим форму поиска
+        text_field = driver.find_element_by_css_selector('.searchW.input')
+        # Передаём в неё автора и название песни
+        text_field.send_keys(f"{author} {title}")
+        
+        # Жмём кнопку поиска
+        driver.find_element_by_class_name('submit-input-wrapped').click()
+        # Выбираем первый результат из списка
+        driver.find_element_by_css_selector('.serpresult :nth-child(2) a').click()
+        
+        # Извлекаем текст песни
+        return driver.find_element_by_id('songLyricsDiv').text
+    finally:
+        driver.quit()
+        
+        
+if __name__ == "__main__":
+    author = input("Введите автора песни > ")
+    title = input("Введите название песни > ")
+    
+    print('Текст песни')
+    print(get_lyrics(author, title))
