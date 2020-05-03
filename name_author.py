3be39353c88a1800510b32429d6793cffea4753c
AlexaEgorova <egorova.a007@gmail.com>
2020-05-03 03:24:36 +0300

    connect two windows, give author&name from one to another

--- name_author.py
+++ name_author.py
@@ -2,28 +2,31 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from tone_analyse import ToneAnalyse
 
+# todo: add button try_again
+
 class NameAuthor(QtWidgets.QMainWindow):
     def __init__(self):
         # Call the inherited classes __init__ method
         super().__init__()
         # Load the .ui file
-        uic.loadUi('main.ui', self)
-        
+        uic.loadUi('main.ui', self)        
         self.analyse_btn = self.findChild(QtWidgets.QPushButton,
                                           'analyseButton')
-        self.analyse_btn.clicked.connect(self.analyseButtonPressed)
-
+        self.analyse_btn.clicked.connect(self.analyseButtonPressed)      
+        self.name_line_edit = self.findChild(QtWidgets.QLineEdit,
+                                             'nameLineEdit')
+        self.author_line_edit = self.findChild(QtWidgets.QLineEdit,
+                                               'authorLineEdit')
         self.show() # Show the GUI
         
     def analyseButtonPressed(self):
         print("analyse button pressed")
         self.hide()
         self.open_new_dialog()
-        #self.child_win = ChildWindow(self)
-        #self.child_win.show()
         
     def open_new_dialog(self):
-        self.nd = ToneAnalyse()
+        self.nd = ToneAnalyse(self.name_line_edit.text(),
+                              self.author_line_edit.text())
         self.nd.show()
 
 if __name__ == "__main__":
--- tone_analyse.py
+++ tone_analyse.py
@@ -1,15 +1,51 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-
+from PIL import ImageQt
+from album_art_finder import find_album_art
+from lyrics_finder import get_lyrics
 
 class ToneAnalyse(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, name, author):
         # Call the inherited classes __init__ method
         super().__init__()
         # Load the .ui file
         uic.loadUi('tone.ui', self)
         print("ready to show tone")
         self.show() # Show the GUI
+        self.name = name
+        print(self.name)
+        self.author = author
+        print(self.author)
+        
+        self.fillAuthor()
+        self.fillName()
+        # self.fillYear()
+        # self.fillArt()
+        # self.getLyrics()
+        
+    def fillArt(self):
+        print("fill art")
+        self.imageLabel = self.findChild(QtWidgets.QLabel, 'imageLabel')
+        image = find_album_art(self.author, self.name)
+        qimage = ImageQt(image)
+        pixmap = QtGui.QPixmap.fromImage(qimage)
+        self.imageLabel.detPixmap(pixmap)
+        
+    def fillYear(self):
+        pass
+    
+    def fillAuthor(self):
+        print("fill author")
+        self.authorLabel = self.findChild(QtWidgets.QLabel, 'authorLabel')
+        self.authorLabel.setText(self.author)
+    
+    def fillName(self):
+        print("fill name")
+        self.nameLabel = self.findChild(QtWidgets.QLabel, 'nameLabel')
+        self.nameLabel.setText(self.name)
+    
+    def getLyrics(self):
+        pass
 
     def showChildWindow(self):
         self.child_win = ChildWindow(self)
